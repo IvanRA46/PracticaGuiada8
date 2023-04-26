4ffdc8167299cccb6d6c8953ce0a05a0531cd578
IvanRA46 <113581493+IvanRA46@users.noreply.github.com>
2023-04-26 11:07:31 -0600

    Add files via upload

---
+++ PracticaGuiada8/src/main/java/com/mycompany/practicaguiada8/PracticaGuiada8.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.practicaguiada8;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PracticaGuiada8 {
+
+    public static void main(String[] args) {
+        Jf principal = new Jf();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+    }
+}
